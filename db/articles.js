4f9b9ef8e9289ae4bb19e14aaa054eeac431aaee
philipwmitilinos-max <philipwmitilinos@gmail.com>
2026-09-22 13:40:58 +0200

    Har flyttat över mina hård kodade task till backend

--- db/articles.js
+++ db/articles.js
@@ -26,8 +26,65 @@ const articles = [
     priority: "low",
     status: "done",
   },
+  {
+    id: 4,
+    title: "Lägg till formulär",
+    description:
+      "Skapa ny task I den här delen ska du skapa ett formulär för att kunna skriva in en ny task. Formuläret ska finnas i en egen komponent. När formuläret skickas ska värdena skrivas ut i konsolen.",
+    assignee: "Philip",
+    category: "Frontend",
+    priority: "low",
+    status: "done",
+  },
+  {
+    id: 5,
+    title: "Laboration del 4",
+    description:
+      "TaskBoard-applikation ska kunna skapa nya Tasks via formuläret",
+    assignee: "Philip",
+    category: "Frontend",
+    priority: "low",
+    status: "done",
+  },
+  {
+    id: 6,
+    title: "Laboration del 5",
+    description: "Implementera en backend-tjänst med node och javascript",
+    assignee: "Philip",
+    category: "Backend",
+    priority: "high",
+    status: "doing",
+  },
+  {
+    id: 7,
+    title: "Laboration del 7",
+    description:
+      "TaskBoard-applikation ska kunna integrera med den backend ni tidigare skapat",
+    assignee: "Philip",
+    category: "Backend",
+    priority: "low",
+    status: "doing",
+  },
+  {
+    id: 8,
+    title: "Laboration del 8",
+    description: "",
+    assignee: "Philip",
+    category: "Backend",
+    priority: "low",
+    status: "todo",
+  },
+  {
+    id: 9,
+    title: "Titel",
+    description: "Beskrivning..",
+    assignee: "Philip",
+    category: "Fronend",
+    priority: "low",
+    status: "done",
+  },
 ];
-let nextId = 4;
+let nextId = 10;
 
 export function getAllTasks() {
   return articles;
